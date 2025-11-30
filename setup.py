c975b10ddcc4b569c047dc000d45c74ae85297a4
rohith-1912 <panathala.rohith@gmail.com>
2025-11-30 12:32:19 +0530

    File Updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 ### Installing Local Package in the environment
+from setuptools import find_packages, setup
+
+setup(
+    name = 'mcqgenerator',
+    version = '0.0.1',
+    author= 'Panathala Rohith',
+    author_email= 'panathala.rohith@gmail.com',
+    install_requires = ["openai", "langchain", "streamlit", "python-dotenv", "PyPDF2"],
+    packages=find_packages()
+)
